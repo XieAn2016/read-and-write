77186ce717f305ac599410d7e20a582e4f6157c5
榭安 <xjysyy@gmail.com>
2018-07-27 16:35:34 +0800

    some

---
+++ shuju.php
@@ -0,0 +1,29 @@
+<?PHP
+$array = array(
+      '0'=>array(
+         'id' =>1, 
+         'title'=> 'simple event', 
+         'start'=> '2018-04-02', 
+         'end'=>'2018-04-15' 
+      ),
+      '1'=>array(
+         'id' =>1, 
+         'title'=> 'simple event', 
+         'start'=> '2018-04-02', 
+         'end'=>'2018-04-15' 
+      ),
+      '2'=>array(
+         'id' =>1, 
+         'title'=> 'simple event', 
+         'start'=> '2018-04-02', 
+         'end'=>'2018-04-15' 
+      )
+  );
+echo $json=json_encode($array);
+
+
+
+
+
+                                                 
+?>
